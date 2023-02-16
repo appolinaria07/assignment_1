1716a710bb95fceece559197cb1263c05053086e
appolinaria07 <paulyna.k@gmail.com>
2023-02-15 20:15:32 -0800

    finished assignment

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// STEP 1 
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+
+// STEP 2
+// 246
+// 'Hello spring!'
+// false
+// null
+
+
+// STEP 3
+// const myString = 'Today will be a good day';
+// alert(myString)
+
+// STEP 4 
+// let strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, blnMayWeContactYou
+
+// STEP 5 
+// let strCity = 'San Diego';
+// let strAddress;
+// strAddress = '1111 JavaScript St, ' + strCity;
+// let numZipCode = '92103', blnMayWeContactYou = true;
+
+// STEP 6 
+// let myParagraph = 'Milky Way is ';
+// console.log(myParagraph + 3 + ' times older than the Earth.')
+
+// STEP 7
+// let var1 = 'Confucius once said that to know what you know and what you do not know, that is ';
+// console.log(var1 + true + ' knowledge.')
+
+// let var2 = 100;
+// console.log(var2 + true)
+
+// STEP 8 
+// To correct the string and display the apostrope after I, \' needs to be added after I:
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9 
+// let myVar = null;
+// console.log(myVar)
+// let myVar2;
+// console.log(myVar2)
+
+// STEP 10 
+// console.log(typeof 'this is my string')
+// console.log(typeof 342)
+// console.log(typeof true)
+// console.log(typeof new Object())
+// console.log(typeof myVar)
+
+// STEP 11
+// alert('Hello ' + 'Polina Kochetkova' + ', welcome to the JavaScript class!')
+
+// STEP 12 
+// let fullName = 'Polina Kochetkova';
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!')
+
+// STEP 13
+// let course = 'JavaScript';
+// alert('Hello ' + fullName + ', welcome to the ' + course + ' class')
+
+// STEP 14
+// alert('Hello Polina Kochetkova.\nWelcome to the JavaScript class!')
+
+
+// STEP 15 
+// let fullName = prompt('Please enter your full name.')
+
+// STEP 16 
+// let courseName = prompt('Please enter the course name.')
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x+y)
+ 
+// STEP 18 
+// let x = 20;
+// x += 20;
+// console.log(x)
+
+// STEP 19 
+// let x = 20;
+// x *= 5;
+// console.log(x)
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x)
+
+// STEP 21 
+// let x = 24, y = 24;
+// console.log(x >= y || x === y)
+
+// STEP 22 
+// let x = 34, y = 0;
+// console.log(x !== y && x <= y)
